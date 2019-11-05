d975dda20bbdd66be22922ef0966fa60d6dea72a
Adrian Haynes <adrianh@adrianh-mbp.spice.spiceworks.com>
2019-11-05 17:27:09 -0600

    learn about accessing state

--- seasons/src/index.js
+++ seasons/src/index.js
@@ -3,15 +3,23 @@ import ReactDOM from "react-dom";
 
 class App extends React.Component {
   constructor(props) {
+    super(props);
+    // NOTE: this is the only time we do direct assignments to this.state
+    this.state = { lat: null };
 
+    window.navigator.geolocation.getCurrentPosition(
+        position => {
+            // we called setState...always have to do this
+            this.setState({ lat: position.coords.latitude });
+            // NEVER DO THIS:
+            // this.state.lat = position.coords.latitude
+        },
+        err => console.log(err)
+      );
   }
   // React says we have to define render
   render() {
-    window.navigator.geolocation.getCurrentPosition(
-      position => console.log(position),
-      err => console.log(err)
-    );
-    return <div>Latitude:</div>;
+    return <div>Latitude: {this.state.lat}</div>;
   }
 }
 
