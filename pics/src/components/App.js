f2c719622472637755a55723be72df268247ff93
Adrian Haynes <adrianhaynes@Adrians-MacBook-Pro.local>
2019-11-09 12:53:14 -0600

    add axios to fetch data

--- pics/src/components/App.js
+++ pics/src/components/App.js
@@ -1,14 +1,26 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 class App extends React.Component {
-  onSearchSubmit(term) {
-    console.log(term);
-  }
+  state = { images: [] };
+
+  onSearchSubmit = async term => {
+    const response = await axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: term },
+      headers: {
+        Authorization:
+          'Client-ID a3f8397d0401a403660795405ca36f634d6e5615cfa2e62243551965cf69cacf'
+      }
+    });
+
+    this.setState({ images: response.data.results });
+  };
   render() {
     return (
       <div className="ui container" style={{ marginTop: '10px' }}>
         <SearchBar onSubmit={this.onSearchSubmit} />
+        Found: {this.state.images.length} images
       </div>
     );
   }
